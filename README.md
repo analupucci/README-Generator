e68b6dfe277c6d8ff43638d5e3bd3439a45c45fa
analupucci <analupucci@gmail.com>
2023-02-22 00:02:59 +0000

    added licenseBadge function

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# ana's readme 
+# d
+
+    ## Project Description
+    undefined
+
+    ## Table of Contents
+    * [Installation](#installation) 
+    * [Usage](#usage) 
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#license)
+    * [Questions](#questions)
+    
+    ## Installation
+    d
+
+    ## Usage
+    d
+
+    ## License
+    d
+
+    ## Contributing
+    d
+
+    ## Tests
+    d
+
+    ## Questions
+    Any questions can be forwarded to:
+    Github:[d](https://github.com/d)
+    or 
+    d
--- index.js
+++ index.js
@@ -62,39 +62,81 @@ const promptUser = () =>
   ]);
 
   const generateREADME = (answers) => {
-    return `# ${titleProject}
+    return `# ${answers.titleProject}
 
     ## Project Description
-    ${projectDescription}
+    ${answers.projectDescription}
 
     ## Table of Contents
-    * [Installation](#installation) 
-    * [Usage](#usage) 
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Tests](#license)
-    * [Questions](#questions)
+    * Installation
+    * Usage
+    * License
+    * Contributing
+    * Tests
+    * Questions
     
     ## Installation
-    ${installationInstructions}
+    ${answers.installationInstructions}
 
     ## Usage
-    ${usageInformation}
+    ${answers.usageInformation}
 
     ## License
-    ${license}
+    ${licenseBadge(license)}
 
     ## Contributing
-    ${contributing}
+    ${answers.contributing}
 
     ## Tests
-    ${tests}
+    ${answers.tests}
 
     ## Questions
     Any questions can be forwarded to:
-    Github:[${questionsGithub}](https://github.com/${questionsGithub})
+    Github:[${answers.questionsGithub}](https://github.com/${answers.questionsGithub})
     or 
-    ${questionsEmail}`
+    ${answers.questionsEmail}`
+
+    function licenseBadge(license){
+
+        if (license === "Apache License 2.0")
+        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+        
+        else if (license === "GNU general Public License v3.0")
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    
+        else if (license === "MIT License")
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT);"
+        
+        else if (license === 'BSD 2 Clause "simplified" License')
+        return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+    
+        else if (license === 'BSD 3 Clause "New" or "revised" License')
+        return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    
+        else if (license === "Boost Software License 1.0" )
+        return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    
+        else if (license === "Creative Commons Zero v1.0 Universal")
+        return "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)";
+    
+        else if (license === "Eclipse Public License 2.0")
+        return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+    
+        else if (license === "GNU affero General Public License v3.0")
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    
+        else if (license === "GNU General Public License v2.0")
+        return "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)" ;
+        
+        else if (license === 'Mozilla Public License 2.0')
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    
+        else if (license === 'The Unlicense')
+        return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+    
+        else if (license === 'none')
+        return "none"
+    
     };
 
 
